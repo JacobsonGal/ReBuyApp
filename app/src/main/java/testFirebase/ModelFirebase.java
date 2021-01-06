630ff2c39097877032956668db5bfde205a2d289
avivhorovitz <avivhorovitz1@gmail.com>
2021-01-06 16:21:17 +0200

    FireBase

---
+++ app/src/main/java/testFirebase/ModelFirebase.java
@@ -0,0 +1,42 @@
+package testFirebase;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
+
+public class ModelFirebase {
+
+
+   public void addUser()
+    {
+        Map<String, Object> user1 = new HashMap<>();
+        user1.put("first", "Ada");
+        user1.put("last", "Lovelace");
+        user1.put("born", 1815);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .add(user1)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/testFirebase/User.java
@@ -0,0 +1,32 @@
+package testFirebase;
+
+public class User {
+
+    private String id;
+    private String name;
+    private String phoneNumber;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
