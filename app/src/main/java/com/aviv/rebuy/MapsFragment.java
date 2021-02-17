19e015c6670e1a365228b43383d9927bac6c46a1
avivhorovitz <avivhorovitz1@gmail.com>
2021-02-17 11:35:39 +0200

    work

--- app/src/main/java/com/aviv/rebuy/MapsFragment.java
+++ app/src/main/java/com/aviv/rebuy/MapsFragment.java
@@ -1,3 +1,4 @@
+/*
 package com.aviv.rebuy;
 
 import androidx.annotation.NonNull;
@@ -20,7 +21,8 @@ public class MapsFragment extends Fragment {
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
-        /**
+        */
+/**
          * Manipulates the map once available.
          * This callback is triggered when the map is ready to be used.
          * This is where we can add markers or lines, add listeners or move the camera.
@@ -28,7 +30,8 @@ public class MapsFragment extends Fragment {
          * If Google Play services is not installed on the device, the user will be prompted to
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
-         */
+         *//*
+
         @Override
         public void onMapReady(GoogleMap googleMap) {
             LatLng sydney = new LatLng(-34, 151);
@@ -54,4 +57,4 @@ public class MapsFragment extends Fragment {
             mapFragment.getMapAsync(callback);
         }
     }
-}
+}*/
