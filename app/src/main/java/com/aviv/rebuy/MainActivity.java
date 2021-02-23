5139227c4e0b27fa6d8d8869335b39efc5f528b5
avivhorovitz <avivhorovitz1@gmail.com>
2021-02-23 15:03:05 +0200

    bottom nav bar

--- app/src/main/java/com/aviv/rebuy/MainActivity.java
+++ app/src/main/java/com/aviv/rebuy/MainActivity.java
@@ -2,21 +2,48 @@ package com.aviv.rebuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.ListFragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import testFirebase.ModelFirebase;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavController navController;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      //  ModelFirebase fr=new ModelFirebase();
-      //  fr.addUser();
+
+        View view=getCurrentFocus();
+         navController = Navigation.findNavController(this , R.id.fragment);
+
+        bottomNavigationView =
+                findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            return navController.navigateUp();
+        }else{
+            return NavigationUI.onNavDestinationSelected(item,navController);
+        }
 
     }
 }
