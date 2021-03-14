94091051ce99ba9b5582f2014f15a469e46b57ae
JacobsonGal <jacobsongal@gmail.com>
2021-03-14 17:59:19 +0200

    Update MapsActivity.java

--- app/src/main/java/com/aviv/rebuy/MapsActivity.java
+++ app/src/main/java/com/aviv/rebuy/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng colman = new LatLng(31.96991544321783, 34.77278720385029);
+        mMap.addMarker(new MarkerOptions().position(colman).title("COLMAN"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(colman));
     }
 }
